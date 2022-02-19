9132160c487de99e63450eddbb4df3fa164694a8
agueroc <agu21022@byui.edu>
2022-02-19 19:10:12 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+let d = new Date();
+
+let year = d.getFullYear();
+
+document.getElementById("year").textContent = year;
+var LastUpdated = document.lastModified;
+
+document.getElementById("lastupdated").innerHTML = LastUpdated.toString();
+
 const datefield = document.querySelector("date");
 const datefieldUK = document.querySelector("aside");
 
